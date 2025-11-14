8bef1e8879d5a6e1e8048bdf0f881788dcce7918
Kevin Szuchet <kevin.szuchet@decentraland.org>
2025-11-14 16:14:36 +0100

    fix: Build and ci

--- src/main.tsx
+++ src/main.tsx
@@ -5,8 +5,11 @@ import { BrowserRouter } from "react-router-dom"
 import ModalProvider from "decentraland-dapps/dist/providers/ModalProvider"
 import TranslationProvider from "decentraland-dapps/dist/providers/TranslationProvider"
 import WalletProvider from "decentraland-dapps/dist/providers/WalletProvider"
+import { createRoot } from "react-dom/client"
 import { DclThemeProvider, darkTheme } from "decentraland-ui2"
-import AppRoutes from "./Routes"
+import { store } from "./app/store"
+import * as locales from "./modules/translation/locales"
+import { AppRoutes } from "./Routes"
 import "./index.css"
 
 const basename = /^decentraland.(zone|org|today)$/.test(window.location.host)
